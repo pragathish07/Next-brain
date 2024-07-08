567d91fb86cadcf02fd6321e11a50498577c76c4
Pragathish <pragarajesh779jd@gmail.com>
2024-07-08 18:14:07 +0530

    authentication using clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://tough-calf-95.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/providers.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+
+
+export function Provider( {children} : {children: React.ReactNode}) {
+
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
+
+  return (
+    
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string} >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+   
+  )
+}
