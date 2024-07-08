ecae57d79d61a77f33e6c67f010ee008cd07f4ae
Pragathish <pragarajesh779jd@gmail.com>
2024-07-08 22:28:04 +0530

    setup convex

---
+++ convex/documents.ts
@@ -0,0 +1,23 @@
+import { mutation,query } from "./_generated/server";
+import { v } from "convex/values";
+ 
+
+export const getDocuments = query({
+    async handler(ctx) {
+        return await ctx.db.query("documents").collect()
+    },
+});
+
+export const createDocuments = mutation({
+    args:{
+        title: v.string(),
+        
+    },
+    async handler(ctx, args) {
+        await ctx.db.insert("documents", {
+            title: args.title,
+           
+        });
+    },
+ 
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 'use client'
 
-import { Authenticated, Unauthenticated } from "convex/react";
+import { Authenticated, Unauthenticated, useQuery } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { api } from "../../convex/_generated/api";
+import { useMutation } from "convex/react";
 
 export default function Home() {
+
+  const createDocument = useMutation(api.documents.createDocuments);
+  const getDocuments = useQuery(api.documents.getDocuments);
+
+
   return (
     <div>
       <h1>Home</h1>
@@ -13,6 +20,11 @@ export default function Home() {
       <Authenticated>
         <UserButton />
         <h1>user</h1>
+        <button onClick={() => createDocument({ title: "test" })}>Create Document</button>
+
+        {getDocuments?.map((document) => (
+          <div key={document._id}>{document.title}</div>
+        ))}
       </Authenticated>
     </div>
   );
